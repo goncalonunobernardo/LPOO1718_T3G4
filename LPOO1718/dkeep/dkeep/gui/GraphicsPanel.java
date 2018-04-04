b6f160c64f272a763621acd2e606226f27361f68
Gonçalo Nuno Bernardo <up201606058@fe.up.pt>
2018-04-04 15:51:57 +0100

    Adding all images

--- LPOO1718/dkeep/dkeep/gui/GraphicsPanel.java
+++ LPOO1718/dkeep/dkeep/gui/GraphicsPanel.java
@@ -38,14 +38,19 @@ public class GraphicsPanel extends JPanel implements KeyListener {
 		
 		try {
 			images.put('H', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/hero_down.png")));
-			images.put('A', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/hero_down.png")));
+			images.put('A', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/hero_armed.png")));
 			images.put('G', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/guard_down.png")));
+			images.put('g', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/guard_asleep.png")));
+			images.put('$', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/ogre_with_key.png")));
+			images.put('8', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/ogre_stunned.png")));
 			images.put('O', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/ogre_down.png")));
 			images.put('*', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/club.png")));
 			images.put('k', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/key.png")));
+			images.put('K', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/hero_with_key.png")));
 			images.put('X', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/wall.png")));
 			images.put(' ', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/free_space.png")));
-			images.put('I', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/free_space.png")));
+			images.put('S', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/free_space.png")));
+			images.put('I', ImageIO.read(GraphicsPanel.class.getResource("/dkeep/res/closed_door.png")));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
