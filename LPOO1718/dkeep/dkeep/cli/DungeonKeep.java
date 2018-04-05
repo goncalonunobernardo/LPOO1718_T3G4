9e6b4930b6b3435dae1bfa02e16b7fb2cf43cf84
anasasilva <anasasousasilva@gmail.com>
2018-04-05 12:31:20 +0100

    Improvements on console game

--- LPOO1718/dkeep/dkeep/cli/DungeonKeep.java
+++ LPOO1718/dkeep/dkeep/cli/DungeonKeep.java
@@ -2,6 +2,7 @@ package dkeep.cli;
 
 import java.util.Scanner;
 import dkeep.logic.Game;
+import dkeep.logic.GameState;
 
 public class DungeonKeep {
 
@@ -9,14 +10,27 @@ public class DungeonKeep {
 		Scanner input = new Scanner (System.in);
 		
 		Game game = new Game();
+		int current_level = game.get_current_level();
 		
 		do {
-			System.out.println(game.get_map());
+			
+			if (game.get_current_level() != current_level) {
+				current_level = game.get_current_level();
+				System.out.println("Nicely done! Playing level " + current_level);
+			}
+			
+			System.out.print(game.get_map());
 			char user_command = input.next().charAt(0);
 			game.play(user_command);
-			game.updateLevel();
 		} while (!game.check_game_over());
-
+		
+		System.out.print(game.get_map());
+		
+		if (game.get_game_status() == GameState.LOST)
+			System.out.println("You lost!");
+		else 
+			System.out.println("You won!");
+			
 		input.close();
 	}
 }
